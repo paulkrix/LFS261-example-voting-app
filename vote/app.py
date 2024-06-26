6eabfcdfd0c9d960fb46539190e86161631f2f66
Paul Krix <paul.krix@gmail.com>
2024-06-26 13:28:13 +1000

    Added deployment

--- vote/app.py
+++ vote/app.py
@@ -5,7 +5,7 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
+option_a = os.getenv('OPTION_A', "Catss")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 version = 'v1'
